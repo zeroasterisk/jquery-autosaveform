93d52ab4dfd190bb3c2c5b1d26401dadcc61f237
alan bount <alan@zeroasterisk.com>
2013-09-05 17:13:41 -0400

    much simpler variant

--- jquery.autosaveform.js
+++ jquery.autosaveform.js
@@ -1,257 +1,143 @@
 /**
-* jQuery AutoSaveForm
-*
-* @author Alan Blount alan+asf@zeroasterisk.com
-* @version 1.0.2
-* @date 2011.08.09
-* @date 2013.05.07
-* @link https://github.com/zeroasterisk/jquery-autosaveform
-*
-* NO COPYRIGHTS OR LICENSES. DO WHAT YOU LIKE.
-*
-*/
-/*
-### USAGE: setting up any number of forms, default options ###
-
-$("#my-container form").autoSaveForm();
-
-### USAGE: setting up one form by ID, custom options ###
-
-$("#MyForm").autoSaveForm({
-	autosave_frequency: 0, 										# autosave disabled
-	autosave_onclick: "#my-container a[href]:not([rel])",		# custom links to be bound as autosavefirst
-	form_inputs: "select",										# only considering selects in the form
-	attr_data: "data-ASFcustom-serialize",						# customize internal attributes for forms' data
-	attr_status: "data-ASFcustom-status",						# customize internal attributes for forms' status
-	form_response_regex: "Saved"								# save response must contain: Saved
-	form_action: "/myform/submit.php"							# over-ride the submit action for the form (edge case)
-	});
-
-### EXTRA: bind event handlers for callbacks ###
-
-$("#MyForm").autoSaveForm({
-	form_response_regex: "Saved"
-}).bind("autosave_complete", function() {
-	pageTracker._trackEvent("ajax", "autoSaveForm", window.location.href, $(this).attr("action"));
-}).bind("autosave_error", function() {
-	pageTracker._trackEvent("ajax", "autoSaveFormERROR", window.location.href, $(this).attr("action"));
-});
-
-### EXTRA CONFIGURATIONS ###
-
-$.fn.ASF_beforeunload = false; 									# disable onbeforeunload functionality
-$.fn.ASF_beforeunload_message = "are you sure?";				# custom message before unload (if we need to prompt)
-$.fn.ASF_debug = true;											# turn on debug messages in console
-
-*/
-(function ($) {
-	// plugin options defaults (set per form)
-	$.fn.ASF_option_defaults = {
-		autosave_frequency: 500000, // set to number of miliseconds you want autosave to run...  0 to disable // 60 * 5 * 1000 = 5 min
-		autosave_onclick: "body a[href]:not([rel])",
-		form_inputs: "input, select, textarea",
-		attr_data: "data-ASF-serialize",
-		attr_status: "data-ASF-status",
-		form_response_regex: "", // if set, the response must match to continue
-		form_action: "" // would override form.action
-	};
-	// plugin constants we keep track of, you shouldn't change
-	$.fn.ASF_debug = false;
-	$.fn.ASF_beforeunload = true;
-	$.fn.ASF_beforeunload_message = "This page is asking you to confirm that you want to leave - any data you have entered may not be saved.";
-	// primary function, setup forms
-	$.fn.autoSaveForm = function (options) {
-		if (!this.length) {
-			return false;
+ * Configuration:
+ * For any form that you want to autosave, set class="autosave"
+ * If you want to use any interval other than the default (30 seconds) specify the following attribute on the form element: autosave-length="10"
+ * If you want to use an alternate post URL (default is form action + /autosave.json), specify the following attribute on the form element: autosave-url="/some/postback.json"
+ * Create a node with class="autosave-status" to determine where the "Auto-saved 9 seconds ago" text will go... if you don't make one, one will be appended to the form for you
+ * If you want to trigger a premature autosave, call $(form).trigger('autosave')
+ * If you want to specify event handlers to run after autosave completes, define events as such: $(form).on('saved', function(){ });
+ *
+ *
+ * form class
+ *   'autosave' - this turns on autosave functionality
+ * form data
+ *   'autosave-save-interval' - control the autosave interval for this form
+ *   'autosave-status-interval' - control the autosave status update interval for this form
+ *   'autosave-lastsave' - unix timestamp (+ms) at last autosave
+ *   'autosave-save-interval-id' - window.setInterval id
+ *   'autosave-status-interval-id' - window.setInterval id
+ *
+ * form events
+ *   'autosave.Autosave' - actually does the autosave of the form
+ *   'saved.Autosave' - internal "on complete/success" callback
+ *     NOTE: you can define your own 'saved' event to get a custom callback
+ *       triggered on Autosave success.
+ *   'status.Autosave' - updates the status message, eg: saved # seconds ago
+ *   'setup_intervals.Autosave' - (re) sets the intervals for autosaving and updating status
+ */
+var __autosave_config__ = {
+	// interval times in seconds
+	interval_save: 30,
+	interval_status: 1
+};
+var autosave = function() {
+	$('form.autosave').each(function() {
+		var form = this;
+		if ($(form).attr('autosave-setup') == '1') {
+			// Don't re-setup
+			return true;
 		}
-		// build main options before element iteration
-		var options = $.extend({}, $.fn.ASF_option_defaults, options || {});
-		// iterate and reformat each matched element
-		$(this).each(function (i, form) {
-			// setup form
-			$(form).data("ASF_options", options);
-			var formData = $(form).find(options.form_inputs).serialize();
-			$(form).attr(options.attr_data, formData);
-			$(form).attr(options.attr_status, "init");
-			if (parseInt(options.autosave_frequency) > 0) {
-				// ensure form has an ID (how we are going to trigger after setTimeout)
-				if (!$(form).attr("id")) {
-					$(form).attr("id", "ASF"+$(window).data("ASF_forms").length+(new Date).getTime());
-				}
-				// bind an action which re-triggers the setTimeout and triggers the autosave
-				$(form).bind("autosave_periodic.autosaveform", function(e) {
-					options = $(this).data("ASF_options");
-					// are we ready for autosave?
-					if ($(this).data("ASF_periodic")=="ready") {
-						asflog("ASF_periodic: ran", $(this));
-						$(this).trigger('autosave.autosaveform');
-					}
-					// re-trigger
-					setTimeout("$('#"+$(this).attr("id")+"').trigger('autosave_periodic.autosaveform');", options.autosave_frequency);
-				}).trigger("autosave_periodic.autosaveform").data("ASF_periodic", "ready");
-			}
-			// setup form autosave event
-			$(form).bind("autosave.autosaveform", function(e) {
-				if ($(this).ASF_status()=="unchanged") {
-					return true; // no changes
-				}
-				options = $(this).data("ASF_options");
-				// check jQueryTools Validator
-				if ($(this).data("validator")) {
-					if (!$(this).data("validator").checkValidity()) {
-						$(this).attr(options.attr_status, "not valid");
-						return false; // not valid
-					}
-				}
-				// setup form
-				var form_action = $(this).attr("action");
-				if (options.form_action.length > 0) {
-					form_action = options.form_action;
-				}
-				var formData = $(this).find(options.form_inputs).serialize();
-				// save/submit form
-				$.ajax({
-					async: true,
-					cache: false,
-					type: "POST",
-					url: form_action,
-					data: formData,
-					form: $(this),
-					formData: formData,
-					formAction: form_action,
-					success: function(data, textStatus, jqXHR) {
-						options = this.form.data("ASF_options");
-						this.form.attr(options.attr_data, this.formData).attr(options.attr_status, 'saved');
-						if (options.form_response_regex.length > 0) {
-							var regex = new RegExp(options.form_response_regex);
-							if (!data.match(regex)) {
-								asflog("Autosave Complete, Regex Failed", $(form), data, textStatus);
-								this.form.attr(options.attr_status, 'saved-regex-failed');
-							}
-						}
-						this.form.data("autosave_data", data);
-						this.form.trigger("autosave_complete.autosaveform");
-					},
-					error: function() {
-						this.form.trigger("autosave_error.autosaveform");
-					}
-				});
-			});
-			// add form to list of ASF_forms
-			ASF_forms = $(window).data("ASF_forms");
-			if (typeof(ASF_forms)=="object" && ASF_forms.length > 0) {
-				ASF_forms.push(form);
+
+		$(form).on('autosave.Autosave', function() {
+			if ($(form).attr('autosave-url') != undefined) {
+				var url = $(form).attr('autosave-url');
 			} else {
-				ASF_forms = [form];
+				var url = $(form).attr('action');
+				if (!url.match('/\/$/')) {
+					url += '/';
+				}
+				url += 'autosave.json';
 			}
-			$(window).data("ASF_forms", ASF_forms);
-			// activate onclick for specified links (cleaner than relying on unload)
-			$(options.autosave_onclick).bind("click.autosaveform", function(e) {
-				// should we autosave?
-				if ($(this).attr("href").indexOf("#")==0 || $(this).closest(".tool-tabs").size() == 1) {
-					asflog("Clicked on link, was a bookmark or tab", $(this), $(form));
-					return true; // bookmark or tab
+			// form data
+			var data = $(form).serialize();
+			// on success function
+			//   if you want to override, define your own 'saved' callback
+			var success = function() {
+				lastsave = new Date().getTime();
+				$(form).data('autosave-lastsave', lastsave);
+				// trigger 'saved' callback, un-namespaced to support custom events
+				$(form).trigger('saved', true);
+			};
+			$.ajax({
+				method: 'post',
+				url: url,
+				data: data,
+				dataType: 'html',
+				success: function(response, status, xhr) {
+					success();
+				},
+				error: function(xhr, status, error) {
+					if (xhr.status == 302) {
+						// Basically a 200 but jQuery doesn't know that
+						success();
+					} else {
+						console.dir(['autosave error', xhr, status, error]);
+					}
 				}
-				allowClickThrough = true;
-				// trigger autosave
-				ASF_forms = $(window).data("ASF_forms");
-				if ($.isArray(ASF_forms) && ASF_forms.length > 0) {
-					$(ASF_forms).each(function(i, form) {
-						asflog("autosave.click", $(this), $(form));
-						options = $(form).data("ASF_options");
-						if ($(form).ASF_status()=="unchanged") {
-							asflog("Clicked on link, status == unchanged", $(this), $(form));
-						} else {
-							$(form).trigger("autosave");
-						}
-						if (!$(form).ASF_check_status($(form).ASF_status())) {
-							asflog("Click Blocked. Form Status: " + $(form).attr(options.attr_status), $(this), $(form));
-							allowClickThrough = false;
-						}
-					});
-				}
-				// what do we return?
-				if (allowClickThrough) {
-					asflog("Clicked on link", allowClickThrough, $(this), $(form));
-					return true;
-				}
+			});
+		});
+
+		$(form).on('saved.Autosave', function(e, status) {
+			if (status == undefined || status == 'success') {
+				status = true;
+			} else {
+				status = false;
+			}
+			$(form).addClass('saved ' + (status ? 'saved-success' : 'saved-failure'));
+			// trigger status update (so we don't have to wait till the next interval
+			$(form).trigger('status.Autosave');
+		});
+
+		$(form).on('status.Autosave', function(e) {
+			// form = e.target // (not needed)
+			var lastsave = $(form).data('autosave-lastsave');
+			if (typeof lastsave != 'number') {
+				// not yet autosaved... do nothing
 				return false;
-			});
-
-			// If using unload feature, deregister a form from autosaving upon unload
-			if ($.fn.ASF_beforeunload) {
-				$(form).submit(function() {
-					var ASF_forms = $(window).data("ASF_forms");
-					ASF_forms = $.map(ASF_forms, function(x) {
-						return x === form ? null : x;
-					});
-					$(window).data("ASF_forms", ASF_forms);
-				});
 			}
-		});
-		// activate ASF_beforeunload (if not already done)
-		if ($.fn.ASF_beforeunload) {
-			$.fn.ASF_beforeunload = false; // no need to re-initialize
-			$(window).bind("beforeunload.autosaveform", function() {
-				$(window).data("ASF-beforeunload-prompt", $.fn.ASF_beforeunload_message);
-				$(window).data("ASF-beforeunload-status", "init-unload");
-				ASF_forms = $(window).data("ASF_forms");
-				if ($.isArray(ASF_forms) && ASF_forms.length > 0) {
-					$(ASF_forms).each(function(i, form) {
-						options = $(form).data("ASF_options");
-						$(form).trigger("autosave");
-						if (!$(form).ASF_check_status($(form).attr(options.attr_status))) {
-							$(window).data("ASF-beforeunload-status", $(form).attr(options.attr_status));
-						}
-					});
-				}
-				if ($(window).data("ASF-beforeunload-status") == "init-unload") {
-					asflog("beforeunload continues: All Forms Fine");
-					$(window).data("ASF-beforeunload-prompt", "");
-				}
-				if ($(window).ASF_check_status($(window).data("ASF-beforeunload-status"))) {
-					asflog("beforeunload continues: Window Statuses Checkout");
-					$(window).data("ASF-beforeunload-prompt", "");
-				}
-				if ($(window).data("ASF-beforeunload-prompt").length > 0) {
-					asflog("Unload Blocked. Window Statuses: " + $(window).data("ASF-beforeunload-status"));
-					$(window).trigger("beforeunloadfailed");
-					return $(window).data("ASF-beforeunload-prompt");
-				}
-			});
-		}
-		return this;
-	};
-	// helper function to check for "ok" statuses
-	$.fn.ASF_check_status = function (status) {
-		asflog("ASF_check_status", status);
-		return (status == "saved" || status == "unchanged")
-	}
-	// helper function to check the status of the data in the form from the last version
-	$.fn.ASF_status = function () {
-		var newStatus = "unknown";
-		this.each(function (i, form) {
-			options = $(form).data("ASF_options");
-			var newData = $(form).find(options.form_inputs).serialize();
-			if ($(form).attr(options.attr_data)==newData) {
-				newStatus = "unchanged";
+			var seconds = (new Date().getTime() - lastsave) / 1000;
+			if (seconds < 3) {
+				var time = 'just now';
 			} else {
-				newStatus = "changed";
+				var time = '' + parseInt(seconds, 10) + ' seconds ago';
 			}
-			$(form).attr(options.attr_status, newStatus);
+			if ($(form).find('.autosave-status').length < 1) {
+				// Populate the status node if it doesn't exist
+				$(form).append($('<div>').addClass('autosave-status'));
+			}
+			$(form).find('.autosave-status').html('Auto-saved ' + time);
 		});
-		return newStatus;
-	}
-	// helper function for console logging
-	asflog = function() {
-		var identify = '[jquery.autosaveform]';
-		if ($.fn.ASF_debug) {
-			if (window.console && window.console.log)
-				window.console.log(identify, arguments);
-			else if (window.opera && window.opera.postError)
-				window.opera.postError(identify, arguments);
-		}
-		return identify + ' ' + Array.prototype.join.call(arguments,'').replace(/\[object Object\]/gi, '');
-	};
-	//end of closure
-}(jQuery));
+
+		$(form).on('setup_intervals.Autosave', function(e) {
+			// clear existing, if set
+			try {
+				window.clearInterval( $(form).data('autosave-save-interval-id') );
+				window.clearInterval( $(form).data('autosave-status-interval-id') );
+			} catch (ase) {}
+			// determine settings
+			var interval_save = __autosave_config__.interval_save; // Default to 30 seconds
+			if ($(form).data('autosave-save-interval') != undefined) {
+				interval_save = parseInt($(form).data('autosave-save-interval'));
+			}
+			if (interval_save < 3 || isNaN(interval_save)) {
+				interval_save = 3;
+			}
+			var interval_status = __autosave_config__.interval_status; // Default to 30 seconds
+			if ($(form).data('autosave-status-interval') != undefined) {
+				interval_status = parseInt($(form).data('autosave-status-interval'));
+			}
+			if (interval_status < 1 || isNaN(interval_status)) {
+				interval_status = 1;
+			}
+			// set
+			$(form).data('autosave-save-interval-id', window.setInterval(function() { try { $(form).trigger('autosave.Autosave'); } catch(e) {} }, interval_save * 1000));
+			$(form).data('autosave-status-interval-id', window.setInterval(function() { try { $(form).trigger('status.Autosave'); } catch(e) {} }, interval_status * 1000));
+		});
+
+		// finalize setup
+		$(form).trigger('setup_intervals.Autosave');
+		$(form).attr('autosave-setup', '1');
+	});
+};
+
+$(autosave);
